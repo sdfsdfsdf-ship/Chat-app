e2ddda1f7401f7d8e39eb012277641c2bbc0e8d6
Rohit Pathak <rohitpathak13032001@gmail.com>
2025-10-09 15:07:32 +0530

    modified env

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,4 +1,13 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000"); // ✅ Directly connect to server
+const SOCKET_URL =
+  process.env.NEXT_PUBLIC_APP_URL || 
+  (typeof window !== "undefined" && window.location.origin) ||
+  "http://localhost:3000";
+
+const socket = io(SOCKET_URL, {
+  path: "/api/socket/io", // Important: match your Socket.IO path
+  transports: ["websocket"], // optional optimization
+});
+
 export default socket;
